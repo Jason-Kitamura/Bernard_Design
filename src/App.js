6fb198788876fef21ef227cfec5ac10fcdef48e7
Jason-Kitamura <jasonkitamura@gmail.com>
2020-08-28 10:18:54 -0400

    basic site started, background and header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <navBar id='navBar' class='navbar'>
+          <row class='row'>
+            <div class='col-6' id='title'>
+              Bernard Design
+            </div>
+            <div class='col-6'>
+              <div class='row'>
+                <div class='col-3 navButton'>
+                  Home
+                </div>
+                <div class='col-3 navButton'>
+                  Gallery
+                </div>
+                <div class='col-3 navButton'>
+                  About
+                </div>
+                <div class='col-3 navButton'>
+                  Contact
+                </div>
+              </div>
+            </div>
+          </row>
+        </navBar>
+      </div>
+      
     </div>
   );
 }
