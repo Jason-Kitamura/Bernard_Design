6dce386046332e0108ec8bd15a55c5aa632e09c9
Jason-Kitamura <jasonkitamura@gmail.com>
2020-08-30 13:07:07 -0400

    home section complete with logo and picture changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/bernardLogo.png'
+import samplePhoto from './assets/livingRoomSample.jpg'
+import leaf from './assets/leaf.png'
 
 function App() {
+  const coverPhoto = {
+    backgroundImage : samplePhoto
+  }
   return (
     <div className="App">
       <div>
         <navBar id='navBar' class='navbar'>
           <row class='row'>
-            <div class='col-6' id='title'>
-              Bernard Design
+            <div class='col-6' >
+              <div class='row'>
+              <div class='col-4'>
+                <img src={logo} id='logo'></img>
+                </div>
+                
+                <div class='col-8'>
+                {/* <p id='title'>
+                  Bernard 
+                  <br/> Design & Build
+                </p> */}
+                </div>
+               
+              </div>
             </div>
             <div class='col-6'>
               <div class='row'>
@@ -28,6 +46,34 @@ function App() {
             </div>
           </row>
         </navBar>
+        <div id='coverArea' style={coverPhoto}>
+          <div class='row'>
+            <div class='col'>
+                <p id='coverTitle'>
+                  BERNARD
+                  <br/> <p>Design <img id='leaf' src={leaf}></img> Build</p>
+                </p>
+            </div>
+            <div class='col'>
+              <p id='coverDes'>
+                WELCOME
+                <br/>
+                  <p>
+                    Quality Work
+                    <br/> Building Trust
+                  </p>
+                <br/>Please Contact
+                <br/>Grayson Bernard
+                <br/> at bernardbuild@gmail.com
+                <br/>(519) 215- 0437 or
+                <br/>(705) 888- 0059
+              </p>
+              
+            </div>
+          </div>
+                {/* <img id='coverPhoto' src={coverPhoto}></img> */}
+
+        </div>
       </div>
       
     </div>
