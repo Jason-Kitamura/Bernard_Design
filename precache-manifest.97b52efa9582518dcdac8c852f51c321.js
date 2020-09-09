d50abe65a2ab17c5704f925124a4c6620b280964
Jason-Kitamura <jasonkitamura@gmail.com>
2020-09-09 11:59:49 -0400

    Updates

---
+++ precache-manifest.97b52efa9582518dcdac8c852f51c321.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a608ed821989c26a46f753f0e60b03c",
+    "url": "/Bernard_Design/index.html"
+  },
+  {
+    "revision": "0b64fe83418ae6493072",
+    "url": "/Bernard_Design/static/css/main.a6e41a4e.chunk.css"
+  },
+  {
+    "revision": "ea4fec17e2f7a21569b5",
+    "url": "/Bernard_Design/static/js/2.21a7aeff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Bernard_Design/static/js/2.21a7aeff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b64fe83418ae6493072",
+    "url": "/Bernard_Design/static/js/main.a9464ed6.chunk.js"
+  },
+  {
+    "revision": "9fba642b46dd9f84a16e",
+    "url": "/Bernard_Design/static/js/runtime-main.162b4526.js"
+  },
+  {
+    "revision": "0bf5b029b771ccf8ec411f7d360e0d5e",
+    "url": "/Bernard_Design/static/media/IMG_00 (1).0bf5b029.jpg"
+  },
+  {
+    "revision": "809db576c98a64e9fc2ac0478ef461e2",
+    "url": "/Bernard_Design/static/media/IMG_00 (10).809db576.jpg"
+  },
+  {
+    "revision": "4c02c2bb04211a6d3d7682b534c5e605",
+    "url": "/Bernard_Design/static/media/IMG_00 (11).4c02c2bb.jpg"
+  },
+  {
+    "revision": "c66ff5aa13c05ad7c175737fc7bcce91",
+    "url": "/Bernard_Design/static/media/IMG_00 (12).c66ff5aa.jpg"
+  },
+  {
+    "revision": "bf8233b79cc22af386cb006afb489b97",
+    "url": "/Bernard_Design/static/media/IMG_00 (13).bf8233b7.jpg"
+  },
+  {
+    "revision": "6c43908c901848aec91c139054e4eac8",
+    "url": "/Bernard_Design/static/media/IMG_00 (14).6c43908c.jpg"
+  },
+  {
+    "revision": "18cbf2b8d5b40446c996609a39573187",
+    "url": "/Bernard_Design/static/media/IMG_00 (15).18cbf2b8.jpg"
+  },
+  {
+    "revision": "b8542bd4f76390cf5499dd9aeb0ac916",
+    "url": "/Bernard_Design/static/media/IMG_00 (16).b8542bd4.jpg"
+  },
+  {
+    "revision": "27170759198e9c6a3e86fd701bdf9fd6",
+    "url": "/Bernard_Design/static/media/IMG_00 (17).27170759.JPG"
+  },
+  {
+    "revision": "b7fb40383a6e0da3306d0296ab04591b",
+    "url": "/Bernard_Design/static/media/IMG_00 (18).b7fb4038.JPG"
+  },
+  {
+    "revision": "bad0ad19b5d35a5ab7b11fa13df1b5a1",
+    "url": "/Bernard_Design/static/media/IMG_00 (19).bad0ad19.JPG"
+  },
+  {
+    "revision": "bd854243dbd8da9da32b597a642788fa",
+    "url": "/Bernard_Design/static/media/IMG_00 (2).bd854243.jpg"
+  },
+  {
+    "revision": "7c2860aa16666fd6c5cad7e23ded3947",
+    "url": "/Bernard_Design/static/media/IMG_00 (20).7c2860aa.JPG"
+  },
+  {
+    "revision": "9d3a19d341c4b7de8885976d7c970d7b",
+    "url": "/Bernard_Design/static/media/IMG_00 (21).9d3a19d3.JPG"
+  },
+  {
+    "revision": "18d2ea691475fb0048177a50c20f96c7",
+    "url": "/Bernard_Design/static/media/IMG_00 (22).18d2ea69.jpg"
+  },
+  {
+    "revision": "7843f7868c6e3001a4cbbb730ac30659",
+    "url": "/Bernard_Design/static/media/IMG_00 (23).7843f786.jpg"
+  },
+  {
+    "revision": "bf93afe8ec79a9ff2d9f06d8d8b9140d",
+    "url": "/Bernard_Design/static/media/IMG_00 (24).bf93afe8.jpg"
+  },
+  {
+    "revision": "7416fd1bd60f57a2f2c2fabf6f62e33a",
+    "url": "/Bernard_Design/static/media/IMG_00 (25).7416fd1b.JPG"
+  },
+  {
+    "revision": "81964ab7731760094b396fe4d22017b9",
+    "url": "/Bernard_Design/static/media/IMG_00 (3).81964ab7.jpg"
+  },
+  {
+    "revision": "09f5ef53baadb7cc79c50bbf8cddbb07",
+    "url": "/Bernard_Design/static/media/IMG_00 (4).09f5ef53.JPG"
+  },
+  {
+    "revision": "9b861e8f33e8442578d98d0e61f911bc",
+    "url": "/Bernard_Design/static/media/IMG_00 (5).9b861e8f.JPG"
+  },
+  {
+    "revision": "e3425d3b87de1a147364e09993dc7657",
+    "url": "/Bernard_Design/static/media/IMG_00 (6).e3425d3b.JPG"
+  },
+  {
+    "revision": "abd149d8b95aadeb58da0de3ca8f45e5",
+    "url": "/Bernard_Design/static/media/IMG_00 (7).abd149d8.JPG"
+  },
+  {
+    "revision": "8772400e607365627f3f2f36b4bb516a",
+    "url": "/Bernard_Design/static/media/IMG_00 (8).8772400e.JPG"
+  },
+  {
+    "revision": "4bee9c2e3cbee597a748ccc1003d7f25",
+    "url": "/Bernard_Design/static/media/IMG_00 (9).4bee9c2e.JPG"
+  },
+  {
+    "revision": "09f174f6fc715594073983efb6355224",
+    "url": "/Bernard_Design/static/media/IMG_01 (1).09f174f6.jpg"
+  },
+  {
+    "revision": "97a798b703166a341ccbd3b07bd02c1e",
+    "url": "/Bernard_Design/static/media/IMG_01 (1).97a798b7.jpg"
+  },
+  {
+    "revision": "ade9b5c58b0566fabf5af32c8de5b196",
+    "url": "/Bernard_Design/static/media/IMG_01 (10).ade9b5c5.jpg"
+  },
+  {
+    "revision": "38243f7e9b7bf5e515d7a13717eeb867",
+    "url": "/Bernard_Design/static/media/IMG_01 (11).38243f7e.jpg"
+  },
+  {
+    "revision": "4445cedbc98824e2d71b3afc006f27c2",
+    "url": "/Bernard_Design/static/media/IMG_01 (2).4445cedb.jpg"
+  },
+  {
+    "revision": "e2714e018e1455e4b89616a7f8cdf9fe",
+    "url": "/Bernard_Design/static/media/IMG_01 (2).e2714e01.jpg"
+  },
+  {
+    "revision": "260f4e832ccd57e904ff27438a516d45",
+    "url": "/Bernard_Design/static/media/IMG_01 (3).260f4e83.JPG"
+  },
+  {
+    "revision": "b91c55df2080e873c967a8789cb2bb28",
+    "url": "/Bernard_Design/static/media/IMG_01 (3).b91c55df.jpg"
+  },
+  {
+    "revision": "06694b9d5411fbbce8f429295910c862",
+    "url": "/Bernard_Design/static/media/IMG_01 (4).06694b9d.jpg"
+  },
+  {
+    "revision": "517793413d8c86cc5b96781893bdbbdc",
+    "url": "/Bernard_Design/static/media/IMG_01 (4).51779341.JPG"
+  },
+  {
+    "revision": "98602cfaa832ffe4410b39fa4e8c8f13",
+    "url": "/Bernard_Design/static/media/IMG_01 (5).98602cfa.jpg"
+  },
+  {
+    "revision": "d7d10027a9a70fe521bccd2bb510046c",
+    "url": "/Bernard_Design/static/media/IMG_01 (5).d7d10027.jpg"
+  },
+  {
+    "revision": "f76242d7a68ddb41473ab74a9ec4b95f",
+    "url": "/Bernard_Design/static/media/IMG_01 (6).f76242d7.jpg"
+  },
+  {
+    "revision": "9f4ac9f36f018504ece80c6f48fc964d",
+    "url": "/Bernard_Design/static/media/IMG_01 (7).9f4ac9f3.jpg"
+  },
+  {
+    "revision": "f8309aa55f8f676926381fe9c17ec42f",
+    "url": "/Bernard_Design/static/media/IMG_01 (7).f8309aa5.jpg"
+  },
+  {
+    "revision": "ac52ca3ba6d6333b793f250116f3107c",
+    "url": "/Bernard_Design/static/media/IMG_01 (8).ac52ca3b.jpg"
+  },
+  {
+    "revision": "53c8513b64816a565dcefc317af8aaac",
+    "url": "/Bernard_Design/static/media/IMG_01 (9).53c8513b.JPG"
+  },
+  {
+    "revision": "40b249afcdfa6d5d16c9b774055a5d31",
+    "url": "/Bernard_Design/static/media/IMG_02 (1).40b249af.jpg"
+  },
+  {
+    "revision": "0c81233ec707ab258667e216bee3dc5c",
+    "url": "/Bernard_Design/static/media/IMG_02 (2).0c81233e.JPG"
+  },
+  {
+    "revision": "21f3db47f2ea1582024dd607243ae245",
+    "url": "/Bernard_Design/static/media/IMG_02 (3).21f3db47.JPG"
+  },
+  {
+    "revision": "646a8bf07f5bfab403ab852bba6bcad2",
+    "url": "/Bernard_Design/static/media/IMG_02 (4).646a8bf0.JPG"
+  },
+  {
+    "revision": "b22d07181d3f3f71c3980326c532a918",
+    "url": "/Bernard_Design/static/media/IMG_02 (5).b22d0718.JPG"
+  },
+  {
+    "revision": "87ddd45fd9e58bd4f0a92511fcb26835",
+    "url": "/Bernard_Design/static/media/IMG_02 (6).87ddd45f.JPG"
+  },
+  {
+    "revision": "8c70b0efa825a42f1770a6edd81f4959",
+    "url": "/Bernard_Design/static/media/IMG_02 (7).8c70b0ef.JPG"
+  },
+  {
+    "revision": "9a39d1a3afc0eec80c8379ff664631da",
+    "url": "/Bernard_Design/static/media/IMG_02 (8).9a39d1a3.jpg"
+  },
+  {
+    "revision": "19fb14533cc533c1ff1dd2d519484240",
+    "url": "/Bernard_Design/static/media/IMG_04 (1).19fb1453.jpg"
+  },
+  {
+    "revision": "6dc17e23fc1061b0ef9c8398b1094c22",
+    "url": "/Bernard_Design/static/media/IMG_04 (2).6dc17e23.JPG"
+  },
+  {
+    "revision": "862cdf4d90851bbc16959311f269132b",
+    "url": "/Bernard_Design/static/media/IMG_04 (3).862cdf4d.jpg"
+  },
+  {
+    "revision": "bcc0e8a7d2d41b6a9e74d3f79dee8ff7",
+    "url": "/Bernard_Design/static/media/IMG_05 (1).bcc0e8a7.jpg"
+  },
+  {
+    "revision": "c5a151d25cf1536ffd6890dc1d0354e0",
+    "url": "/Bernard_Design/static/media/IMG_05 (2).c5a151d2.JPG"
+  },
+  {
+    "revision": "93ad8a36b5620d68ac32729d4a8c6e16",
+    "url": "/Bernard_Design/static/media/IMG_05 (3).93ad8a36.JPG"
+  },
+  {
+    "revision": "f232bd67dd78f7757e58e42302757949",
+    "url": "/Bernard_Design/static/media/IMG_05 (4).f232bd67.jpg"
+  },
+  {
+    "revision": "8ce08ff986a76dac9c5e15b95b8e6875",
+    "url": "/Bernard_Design/static/media/IMG_05 (5).8ce08ff9.jpg"
+  },
+  {
+    "revision": "17f6d2adae35466f9429f4eb54af6321",
+    "url": "/Bernard_Design/static/media/IMG_05 (6).17f6d2ad.jpg"
+  },
+  {
+    "revision": "80150106d98af4e42280996e48661e2a",
+    "url": "/Bernard_Design/static/media/WhiteBrickLights.80150106.jpg"
+  },
+  {
+    "revision": "bee2cd28dcf91318733d70ff18d17177",
+    "url": "/Bernard_Design/static/media/bernardLogo.bee2cd28.png"
+  },
+  {
+    "revision": "fd7b8dea764b6513b35bc7e66601e87e",
+    "url": "/Bernard_Design/static/media/fireplace1.fd7b8dea.jpg"
+  },
+  {
+    "revision": "14a644c7b9cb19b02e4873430f6022a8",
+    "url": "/Bernard_Design/static/media/kitchenCover.14a644c7.jpg"
+  },
+  {
+    "revision": "14d8366e3a901b925e882c120087f96e",
+    "url": "/Bernard_Design/static/media/livingRoomSample.14d8366e.jpg"
+  }
+]);
